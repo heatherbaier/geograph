e727548de437fb1810bd677f540962f8ca799259
heatherbaier <hmbaier@email.wm.edu>
2021-06-02 15:44:37 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='geograph',
+    version='0.0.1',
+    description='GeoGraph tools',
+    url='git@github.com:heatherbaier/geograph.git',
+    author='Heather Baier',
+    author_email='hmbaier@email.wm.edu',
+    license='unlicense',
+    packages=['geograph'],
+    zip_safe=False
+)
